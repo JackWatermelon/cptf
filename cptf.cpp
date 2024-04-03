a41662c0b3f072f6b8b6059fd9bc182c3a5fe971
John Flowers <jflo@730-ln11701-r.jeffco.schools>
2024-04-03 09:24:06 -0600

    first commit

---
+++ cptf.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+string January[31];
+string February[28];
+string March[31];
+string April[30];
+string May[31];
+string June[30];
+string July[31];
+string August[31];
+string September[30];
+string October[31];
+string November[30];
+string December[31];
+
+int main(){
+	return 0;
+}
